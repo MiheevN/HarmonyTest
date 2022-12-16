b92629c9bd93f48912b898927d205e262b4d2c04
Nikolai Mikheev <miheevkolya@gmail.com>
2022-12-16 16:15:45 +0700

    Add project files.

---
+++ HarmonyWebTest/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using HarmonyWebTest.Entitys;
+using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace HarmonyWebTest.Controllers
+{
+    //Пока без подтверждений, но понятно что надо возвращать bool = успешно
+    [ApiController]
+    [Route("/[action]")]
+    public class HomeController : Controller
+    {
+
+        [HttpGet()]
+        public string GetItem(string name)
+        {
+            Console.WriteLine($"Requested Get: {name}");
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return string.Empty;
+            }
+            using var DB = new LiteDatabase("TestDB");
+            var entitys = DB.GetCollection<ValueEntity>("entitys");
+            var find = entitys.FindOne(E => E.Name == name);
+            if (find != null)
+            {
+                return find.Value;
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Добавление/обновление записи.
+        /// Если запись с таким именем уже существует, она обновится
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        [HttpPost()]
+        public void AddItem([FromForm] string name, [FromForm] string value)
+        {
+
+            using var DB = new LiteDatabase("TestDB");
+            var entitys = DB.GetCollection<ValueEntity>("entitys");
+            var find = entitys.FindOne(E => E.Name == name);
+
+            if (find == null)
+            {
+                Console.WriteLine($"Requested Add: {name}");
+                entitys.Insert(new ValueEntity()
+                {
+                    Name = name,
+                    Value = value
+                });
+            }
+            else
+            {
+                Console.WriteLine($"Requested Update: {name}");
+                find.Value = value;
+                entitys.Update(find);
+            }
+        }
+
+        [HttpPost()]
+        public void UpdateItem(string name, string value)
+        {
+            AddItem(name, value);//В добавлении всё равно обновляется 
+        }
+
+        [HttpDelete()]
+        public void DeleteItem(string name)
+        {
+            Console.WriteLine($"Requested Delete: {name}");
+
+            using var DB = new LiteDatabase("TestDB");
+            var entitys = DB.GetCollection<ValueEntity>("entitys");
+            var find = entitys.FindOne(E => E.Name == name);
+            if (find != null)
+            {
+                entitys.Delete(find.id);
+            }
+        }
+    }
+}
---
+++ WpfHarmony/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHarmony
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string Host = "http://localhost:5092";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            EditNameText.Text = FindNameText.Text;
+            EditValueText.Text = FindValueText.Text;
+        }
+
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            Add();
+        }
+
+        private void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (FindValueText.Text.Length > 0)
+                Delete();
+            FindNameText.Text = string.Empty;
+            FindValueText.Text = string.Empty;
+        }
+
+        private void FindNameText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FindNameText.Text.Length > 0)
+            {
+                Search();
+            }
+            else
+            {
+                FindValueText.Text = string.Empty;
+            }
+        }
+        private async Task Search()
+        {
+            var http = new HttpClient();
+            var result = await http.GetStringAsync($"{Host}/GetItem?name={FindNameText.Text}");
+
+            if (string.IsNullOrEmpty(result))
+            {
+                //Подписать уведомление
+                return;
+            }
+            FindValueText.Text = result;
+        }
+        private async Task Add()
+        {
+            var http = new HttpClient();
+            var Response = await http.PostAsync($"{Host}/AddItem", new FormUrlEncodedContent(new Dictionary<string, string>()
+            {
+                { "name", EditNameText.Text },
+                { "value", EditValueText.Text }
+            }
+            ));
+            Console.WriteLine(await Response.Content.ReadAsStringAsync());
+        }
+        private async Task Delete()
+        {
+            var http = new HttpClient();
+            await http.DeleteAsync($"{Host}/DeleteItem?name={FindNameText.Text}");
+        }
+
+    }
+}
